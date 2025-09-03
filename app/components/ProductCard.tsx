a5f815ba73b942a32dc463dfb44cf9b85d8c64a9
Mohd Firoj <firojqv@gmail.com>
2025-09-03 16:36:33 +0530

    css modules

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import AddToCart from './AddToCart';
+import styles from './ProductCart.module.css'
 
 const ProductCard = () => {
     return (
-        <div>
+        <div className={styles.card}>
             <AddToCart/>
         </div>
     )
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -4,7 +4,9 @@ interface User {
     name: string;
 }
 const UsersPage = async () => {
-     const res = await fetch('https://jsonplaceholder.typicode.com/users');   //caching come automatically with Fetch in NEXT.JS
+    const res = await fetch('https://jsonplaceholder.typicode.com/users',{
+        cache:'no-store'
+    });   //caching come automatically with Fetch in NEXT.JS
     // const res = await fetch(
     //     'https://jsonplaceholder.typicode.com/users',
     //     {cache:'no-cache'});                           // Disabling the caching
@@ -16,6 +18,7 @@ const UsersPage = async () => {
     return (
         <>
             <h1>Users</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
             <ul>
                 {users.map(user => <li key={user.id}>{user.name}</li>)}
             </ul>
